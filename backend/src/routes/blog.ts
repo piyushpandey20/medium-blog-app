8012d20f2ecffb636c4d61e6e71a380c3d3b4fd3
Piyush Pandey <piyush2003pandey@gmail.com>
2024-10-01 20:47:21 +0530

    added frontend and some type errors resolve

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -99,7 +99,18 @@ blogRouter.get("/bulk", async(c) => {
     const prisma = new PrismaClient({
         datasourceUrl: c.env?.DATABASE_URL,
     }).$extends(withAccelerate());
-    const blogs = await prisma.post.findMany();
+    const blogs = await prisma.post.findMany({
+        select:{
+            content:true,
+            title:true,
+            id:true,
+            author:{
+                select:{
+                    name:true
+                }
+            }
+        }
+    });
   return c.json({
     blogs
   });
@@ -117,6 +128,16 @@ blogRouter.get("/:id", async(c) => {
             where: {
                 id: Number(id)
             },
+            select:{
+                id:true,
+                title:true,
+                content:true,
+                author:{
+                    select:{
+                        name:true
+                    }
+                }
+            }
         })
     
         return c.json({
